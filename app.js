c7989969c18e6f72c301d9b4604ba609c209df65
Leonardo Padilha <lcfpadilha@gmail.com>
2016-07-26 16:25:10 -0300

    tentando arrumar problema de cors

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const app = require('express')();
-const fs = require('fs');
+const app  = require('express')();
+const cors = require('cors');
+const fs   = require('fs');
 const birl = require('./code_exec.js');
 
 // Permitindo CORS
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.get ('/', function (req, res) {
   console.log ('GET AT \'/\'');
   res.send ('HELLO');
 });
 
-app.post('/compile', function (req, res) {
+app.post('/compile', cors (), function (req, res) {
   console.log ('POST AT \'/compile\'');
 
   // Lendo o JSON
@@ -30,22 +27,8 @@ app.post('/compile', function (req, res) {
   });
 });
 
-app.options('/compile', function (req, res) {
-  console.log ('OPTIONS AT \'/compile\'');
-
-  // Lendo o JSON
-  var body = '';
-  req.on('data', function (data) {
-    body += data;
-  });
-
-  // Enviando o código e o stdin para a execução do código
-  req.on('end', function () {
-    var json = JSON.parse(body);
-    birl(json.code, json.stdin, res);
-  });
-});
+app.options('/compile', cors());
 
 var port = process.env.PORT || 3000;
 app.listen (port);
-console.log ('Listening at https://localhost:' + port);
+console.log ('Listening at http://localhost:' + port);
