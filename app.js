6f0d4836e1ee07305168ab5302cab2a947c0d0f6
Leonardo Padilha <lcfpadilha@gmail.com>
2016-07-26 15:38:47 -0300

    inserindo express

--- app.js
+++ app.js
@@ -1,52 +1,25 @@
-/*const https = require('https');
+const app = require('express')();
 const fs = require('fs');
 const birl = require('./code_exec.js');
+app.get ('/', function (req, res) {
+  console.log ('GET AT \'/\'')
+})
+app.post('/compile', function (req, res) {
+  console.log ('POST AT \'/compile\'');
 
-var options = { 
-    key:  fs.readFileSync('server.key'), 
-    cert: fs.readFileSync('server.crt')
-}; 
+  // Lendo o JSON
+  var body = '';
+  req.on('data', function (data) {
+    body += data;
+  });
 
-const server = https.createServer(options, function(req, res) {
-    console.log ("req");
-   if (req.url == "/compile" && req.method == 'POST') {
-      console.log("POST");
-
-      // Lendo o JSON
-      var body = '';
-      req.on('data', function (data) {
-         body += data;
-      });
-
-      // Enviando o código e o stdin para a execução do código
-      req.on('end', function () {
-         var json = JSON.parse(body);
-         birl(json.code, json.stdin, res);
-     });
-   }
-   else {
-      res.writeHead(404, {"Content-Type": "text/plain"});
-      res.write("404 Not Found\n");
-      res.end();
-   }
+  // Enviando o código e o stdin para a execução do código
+  req.on('end', function () {
+    var json = JSON.parse(body);
+    birl(json.code, json.stdin, res);
+  });
 });
 
-var port = process.env.PORT || 8080;
-server.listen(port);
-console.log('Listening at https://localhost:' + port);*/
-
-var http = require('http');
-var express = require("express");
-
-var app = express();
-
-app.set('port', process.env.PORT || 3000);
-
-app.get('/', function(request, response) {
-  console.log('[support dash] processing get request')
-  response.send('Hello World 2!');
-});
-
-app.listen(process.env.PORT, function () {
-  console.log('***** exp listening on port: ' + process.env.PORT);
-});
+var port = process.env.PORT || 3000;
+app.listen (port);
+console.log ('Listening at https://localhost:' + port);
