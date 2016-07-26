ec55436ccb420e8023bb138fa3fd79ca93f8ede0
Leonardo Padilha <lcfpadilha@gmail.com>
2016-07-26 16:04:06 -0300

    arrumando url

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const birl = require('./code_exec.js');
 
 // Permitindo CORS
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', 'https://birl.herokuapp.com/compile');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
