cceade900232a37f7a6816f6469ba493f338b2d0
Leonardo Padilha <lcfpadilha@gmail.com>
2016-07-26 16:02:40 -0300

    arrumando problema com CORS

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const app = require('express')();
 const fs = require('fs');
 const birl = require('./code_exec.js');
+
+// Permitindo CORS
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.get ('/', function (req, res) {
   console.log ('GET AT \'/\'');
   res.send ('HELLO');
@@ -8,6 +18,22 @@ app.get ('/', function (req, res) {
 
 app.post('/compile', function (req, res) {
   console.log ('POST AT \'/compile\'');
+
+  // Lendo o JSON
+  var body = '';
+  req.on('data', function (data) {
+    body += data;
+  });
+
+  // Enviando o código e o stdin para a execução do código
+  req.on('end', function () {
+    var json = JSON.parse(body);
+    birl(json.code, json.stdin, res);
+  });
+});
+
+app.options('/compile', function (req, res) {
+  console.log ('OPTIONS AT \'/compile\'');
 
   // Lendo o JSON
   var body = '';
