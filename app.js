5c0d4184c3bb22e86c33569d8156cc2a38675330
Leonardo Padilha <lcfpadilha@gmail.com>
2016-07-26 16:11:24 -0300

    tentando resolver problema de cors

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const fs = require('fs');
 const birl = require('./code_exec.js');
 
 // Permitindo CORS
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://birl-language.github.io/');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
